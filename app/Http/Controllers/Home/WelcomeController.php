4e48b2cd433ca77554c784569561f81508c89ce0
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2026-03-11 22:19:08 +0300

    jobs being displayed

--- app/Http/Controllers/Home/WelcomeController.php
+++ app/Http/Controllers/Home/WelcomeController.php
@@ -10,16 +10,17 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        
         // Get URL from config
         $mainAppUrl = config('api.main_app.api_base');
         
         // Get data from main app
         $response = Http::get($mainAppUrl . '/user-data');
         
-        // Convert to array
+        // Convert to array with null check
         $data = $response->json();
-        // dd($data);
+        
+        // Ensure $data is an array (default to empty array if null)
+        $data = is_array($data) ? $data : [];
         
         // Pass to view
         return view('home.welcome', $data);
